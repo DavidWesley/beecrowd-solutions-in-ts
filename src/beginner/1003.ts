8b6bc0392f3b4b608ceb65421821a406717e35df
David Wesley <contato.dwesley@outlook.com>
2023-08-13 17:19:32 -0300

    feat: 1003 - Simple Sum

---
+++ src/beginner/1003.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from "node:fs"
+import { EOL } from "node:os"
+import { stdin } from "node:process"
+
+const [A, B] = readFileSync(stdin.fd, { encoding: "ascii" })
+  .split(EOL, 2)
+  .map((value) => Number.parseInt(value, 10)) as [number, number]
+
+function main(): void {
+  console.log("SOMA = %d", A + B)
+}
+
+main()
