523b8c4e6a018cca7ba320104f3f9c434e236bd4
David Wesley <contato.dwesley@outlook.com>
2023-09-30 22:52:59 -0300

    feat: 1060 - Positive Numbers

---
+++ src/beginner/1060.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from "node:fs"
+import { EOL } from "node:os"
+import { stdin } from "node:process"
+
+const input = readFileSync(stdin.fd, { encoding: "ascii" })
+  .split(EOL, 6)
+  .map(Number) as [number, number, number, number, number, number]
+
+/**
+ * A higher-order function that takes a predicate function and returns a new function.
+ * The returned function takes an arbitrary number of values and counts how many of them
+ * satisfy the predicate function.
+ *
+ * @param {function} fn - The predicate function used to determine if a value satisfies a condition.
+ * @return {function} A function that takes an arrays of numbers and returns the count of values that satisfy the predicate function.
+ */
+const counter = <T>(fn: (arg0: T) => boolean) => {
+  return (values: Array<T>): number => {
+    let count = 0
+    for (const value of values)
+      if (fn(value))
+        count += 1
+
+    return count
+  }
+}
+
+const isPositive = (value: number): boolean => value > 0
+const positiveNumbersCounter = counter(isPositive)
+
+function main(): void {
+  console.log("%d valores positivos", positiveNumbersCounter(input))
+}
+
+main()
